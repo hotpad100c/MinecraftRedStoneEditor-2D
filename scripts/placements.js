34fdc21f397e3a673afc96f1d52ff15a5aea91d4
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-10-08 07:52:06 +0900

    Update placements.js

--- scripts/placements.js
+++ scripts/placements.js
@@ -1,13 +1,16 @@
 function setBlock(gridX, gridY, selectedComponent){
-  const key = `${gridX},${gridY}`;
-  if (grid[key] === selectedComponent) {
-      delete grid[key];
-  } else if (selectedComponent !== 'air') {
-      grid[key] = selectedComponent;
+  if (gridX >= 0 && gridX < canvasSize && gridY >= 0 && gridY < canvasSize) {
+    const key = `${gridX},${gridY}`;
+    if (grid[key] === selectedComponent) {
+        delete grid[key];
+    } else if (selectedComponent !== 'air') {
+        grid[key] = selectedComponent;
+    }
+    checkConnectedPlacement(gridX, gridY, selectedComponent);
+    hasChanges = true;
+    updateStatusBar();
   }
-  hasChanges = true;
-  updateStatusBar();
 }
-function checkConnectedPlacement(){
+function checkConnectedPlacement(x,y,block){
   
 }
