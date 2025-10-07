987b73ad93502c4bff2170ce4fd3d8a23167edd1
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-10-08 07:48:52 +0900

    Update placements.js

--- scripts/placements.js
+++ scripts/placements.js
@@ -1,1 +1,13 @@
-
+function setBlock(gridX, gridY, selectedComponent){
+  const key = `${gridX},${gridY}`;
+  if (grid[key] === selectedComponent) {
+      delete grid[key];
+  } else if (selectedComponent !== 'air') {
+      grid[key] = selectedComponent;
+  }
+  hasChanges = true;
+  updateStatusBar();
+}
+function checkConnectedPlacement(){
+  
+}
